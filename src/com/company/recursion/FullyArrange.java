52b94bf3da39144c25b1336510b78cf248c042d3
qingshanyuluo <qingshanyuluo@gmail.com>
2019-09-12 15:56:15 +0800

    complete generic

--- src/com/company/recursion/FullyArrange.java
+++ src/com/company/recursion/FullyArrange.java
@@ -7,22 +7,23 @@ import java.util.List;
 public class FullyArrange {
 
     public static void main(String[] args) {
-        Integer[] d = {1, 2, 3};
-        List<Comparable> data = Arrays.asList(d);
+        Integer[] d = {1, 2, 3, 4};
+//        List<Comparable> data = Arrays.asList(d);
+        List<Integer> data = Arrays.asList(d);
         System.out.println(getFullyArrange(data));
     }
 
-    public static List<List<Comparable>> getFullyArrange(List<Comparable> data) {
-        List<List<Comparable>> res = new ArrayList<>();
+    public static <T extends Comparable> List<List<T>> getFullyArrange(List<T> data) {
+        List<List<T>> res = new ArrayList<>();
         getArrange(data, 0, data.size(), res);
         return res;
     }
 
-    private static void getArrange(List<Comparable> data, int start, int end, List<List<Comparable>> res) {
+    private static <T extends Comparable> void getArrange(List<T> data, int start, int end, List<List<T>> res) {
         if (start == end) {
             res.add(data);
         } else {
-            Comparable temp;
+            T temp;
             for (int i = start; i < end; i++) {
                 data = new ArrayList<>(data);
                 temp = data.get(start);
@@ -32,5 +33,25 @@ public class FullyArrange {
             }
         }
     }
+//     public static List<List<Comparable>> getFullyArrange(List<Comparable> data) {
+//            List<List<Comparable>> res = new ArrayList<>();
+//            getArrange(data, 0, data.size(), res);
+//            return res;
+//        }
+//
+//        private static void getArrange(List<Comparable> data, int start, int end, List<List<Comparable>> res) {
+//            if (start == end) {
+//                res.add(data);
+//            } else {
+//                Comparable temp;
+//                for (int i = start; i < end; i++) {
+//                    data = new ArrayList<>(data);
+//                    temp = data.get(start);
+//                    data.set(start, data.get(i));
+//                    data.set(i, temp);
+//                    getArrange(data, start + 1, end, res);
+//                }
+//            }
+//        }
 
 }
